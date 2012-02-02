550b2b895954e76f52b08bac58b47095bd35e47c
Mark Badolato <mbadolato@cainandassociates.com>
2012-02-01 18:15:10 -0700

    Add support for skipping blank lines
    If a CSV file begins with a blank line (before the header), the EasyCSV library will bomb. This is because fgetcsv() returns an array with a single item (with a value of null) when it encounters a blank line. So, in a case where the 1st line is blank, the header row get set to a one-item array. When the 1st real line of data is read (the header row), the array size of the row will not match the single-item array size of $this->_headers, and we get an error.

--- lib/EasyCSV/Reader.php
+++ lib/EasyCSV/Reader.php
@@ -6,17 +6,30 @@ class Reader extends AbstractBase
 {
     private $_headers;
     private $_line;
+    private $_skipBlanks;
 
-    public function __construct($path, $mode = 'r+')
+    public function __construct($path, $mode = 'r+', $skipBlanks = true)
     {
         parent::__construct($path, $mode);
-        $this->_headers = $this->getRow();
-        $this->_line    = 0;
+        $this->_skipBlanks = $skipBlanks;
+        $this->_headers    = $this->getRow();
+        $this->_line       = 0;
     }
 
     public function getRow()
     {
         if (($row = fgetcsv($this->_handle, 1000, $this->_delimiter, $this->_enclosure)) !== false) {
+            if (true === $this->_skipBlanks) {
+                while ((1 == count($row)) && (null == $row[0]))
+                {
+                    $this->_line++;
+
+                    if (($row = fgetcsv($this->_handle, 1000, $this->_delimiter, $this->_enclosure)) === false) {
+                        return false;
+                    }
+                }
+            }
+
             $this->_line++;
             return $this->_headers ? array_combine($this->_headers, $row) : $row;
         } else {
