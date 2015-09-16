5ba5e1990bbbc4ab65d491e120680da1f8f26826
clzola <lazar.radinovic@gmail.com>
2015-09-16 10:16:34 +0200

    Added option for csv header.

--- lib/EasyCSV/AbstractBase.php
+++ lib/EasyCSV/AbstractBase.php
@@ -8,13 +8,21 @@ abstract class AbstractBase
     protected $delimiter = ',';
     protected $enclosure = '"';
 
-    public function __construct($path, $mode = 'r+')
+    public function __construct($path, $mode = 'r+', $headers = array())
     {
+        $newfile = false;
         if (! file_exists($path)) {
             touch($path);
+            $newfile = true;
         }
+        
         $this->handle = new \SplFileObject($path, $mode);
         $this->handle->setFlags(\SplFileObject::DROP_NEW_LINE);
+        
+        if($newfile && !empty($headers)) {
+			$headerLine = join(',', $headers) . PHP_EOL;
+			$this->handle->fwrite($headerLine, strlen($headerLine));
+		}
     }
 
     public function __destruct()
