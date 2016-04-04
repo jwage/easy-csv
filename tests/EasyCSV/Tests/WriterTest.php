a7680e21851fff5c058cf7665e7368aae85d8579
Arcady Usov <itcrab@ya.ru>
2016-04-04 22:43:17 +0500

    Add tests for writing/reading CSV-file with UTF-8 BOM symbol

--- tests/EasyCSV/Tests/WriterTest.php
+++ tests/EasyCSV/Tests/WriterTest.php
@@ -9,11 +9,17 @@ class WriterTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->writer = new \EasyCSV\Writer(__DIR__ . '/write.csv');
+        $this->writerBOM = new \EasyCSV\Writer(__DIR__ . '/write_bom.csv', 'r+', true);
     }
 
     public function testWriteRow()
     {
         $this->writer->writeRow('test1, test2, test3');
+    }
+
+    public function testWriteBOMRow()
+    {
+        $this->writerBOM->writeRow('колонка 1, колонка 2, колонка 3');
     }
 
     public function testWriteFromArray()
@@ -22,6 +28,15 @@ class WriterTest extends \PHPUnit_Framework_TestCase
         $this->writer->writeFromArray(array(
             '1test1, 1test2ing this out, 1test3',
             array('2test1', '2test2 ing this out ok', '2test3')
+        ));
+    }
+
+    public function testWriteBOMFromArray()
+    {
+        $this->writerBOM->writeRow('колонка 1, колонка 2, колонка 3');
+        $this->writerBOM->writeFromArray(array(
+            'значение 1, значение 2, значение 3',
+            array('значение 4', 'значение 5', 'значение 6')
         ));
     }
 
@@ -39,6 +54,25 @@ class WriterTest extends \PHPUnit_Framework_TestCase
                 'column1' => '2test1',
                 'column2' => '2test2 ing this out ok',
                 'column3' => '2test3'
+            )
+        );
+        $this->assertEquals($expected, $results);
+    }
+
+    public function testReadWrittenBOMFile()
+    {
+        $reader = new \EasyCSV\Reader(__DIR__ . '/write_bom.csv');
+        $results = $reader->getAll();
+        $expected = array(
+            array(
+                'колонка 1' => 'значение 1',
+                'колонка 2' => 'значение 2',
+                'колонка 3' => 'значение 3'
+            ),
+            array(
+                'колонка 1' => 'значение 4',
+                'колонка 2' => 'значение 5',
+                'колонка 3' => 'значение 6'
             )
         );
         $this->assertEquals($expected, $results);
